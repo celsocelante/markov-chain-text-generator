652f08ae481c05f0293c27d6ba5f54630a95fc3b
Celso Celante <celsocelante@gmail.com>
2016-12-05 15:49:06 -0200

    Já consegue gerar os textos aleatórios

--- main.py
+++ main.py
@@ -15,13 +15,35 @@ input_file = open(sys.argv[1], "r")
 corpus = input_file.read().replace('\n', ' ')
 ' '.join(corpus.split())
 
-testando = "testando"
-
 for i in range(0, len(corpus)):
     # Quebra o texto em palavras de tamanho k e adiciona a cadeia
     end = i + k
+
+    # Para o loop quando chega ao fim da string, para evitar violacao
     if end is len(corpus) + 1:
         break
-    print corpus[i: end]
 
-#print(corpus)
+    # Palavra de tamanho k
+    word = corpus[i: end]
+
+    try:
+        # Adiciona a palavra e os proximos caracteres a cadeia
+        if markov_chain.has_key(word):
+            list_chars = markov_chain[word]
+            list_chars.append(corpus[end])
+            markov_chain[word] = list_chars
+        else:
+            list_chars = []
+            list_chars.append(corpus[end])
+            markov_chain[word] = list_chars
+    except IndexError as e:
+        break
+
+final_text = random.choice(markov_chain.keys())
+final_text = final_text + random.choice(markov_chain[final_text])
+
+# Gera os proximos estados ate atingir o tamanho final l
+while len(final_text) < l:
+    final_text = final_text + random.choice(markov_chain[final_text[-k:]])
+
+print final_text
