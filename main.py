8a4a893a023bba5267ff5ed016479639950077a8
Celso Celante <celsocelante@gmail.com>
2016-12-05 13:59:58 -0200

    Quebra o text em palavras de tamanho k

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import sys
 import random
 
+# Chocagem de argumentos
 if not len(sys.argv) == 4:
     print("Usage: " + (sys.argv[0] + " depth length"))
     sys.exit(1)
 
-level = int(sys.argv[2]) # depth k
-length = int(sys.argv[3]) # length l (output)
+k = int(sys.argv[2]) # depth k
+l = int(sys.argv[3]) # length l (output)
 markov_chain = {} # inicializa a cadeia de Markov como um dicionari vazio
 
 # Abre o arquivo, le e remove as quebras de linha e espacos excessivos
@@ -14,4 +15,13 @@ input_file = open(sys.argv[1], "r")
 corpus = input_file.read().replace('\n', ' ')
 ' '.join(corpus.split())
 
-print(corpus)
+testando = "testando"
+
+for i in range(0, len(corpus)):
+    # Quebra o texto em palavras de tamanho k e adiciona a cadeia
+    end = i + k
+    if end is len(corpus) + 1:
+        break
+    print corpus[i: end]
+
+#print(corpus)
